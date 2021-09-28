3c2be41c52d07aac5b85d0c81efc817b3c8a0456
SweetieRick <65025304+SweetieRick@users.noreply.github.com>
2021-09-28 21:17:19 +0200

    Added new woodcutter block

--- src/main/java/sweetierick/craftyitemsmod/Craftyitemsmod.java
+++ src/main/java/sweetierick/craftyitemsmod/Craftyitemsmod.java
@@ -8,6 +8,7 @@ import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import sweetierick.craftyitemsmod.blocks.RosePineBlock;
+import sweetierick.craftyitemsmod.blocks.WoodCutterBlock;
 import sweetierick.craftyitemsmod.items.FabricPieceItem;
 
 /* import java.rmi.registry.Registry; */
@@ -21,7 +22,6 @@ public class Craftyitemsmod implements ModInitializer {
      * simply have useful tooltips to them
      */
     public static final Item FabricPiece = new FabricPieceItem(new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(1));
-    //  public static final Item RosePine = new RosePineItem(new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64));
     /**
      * This second array of items are
      * passive items, or items which have
@@ -31,6 +31,7 @@ public class Craftyitemsmod implements ModInitializer {
     public static final Item ThickString = new Item(new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64));
     public static final Item PineDust = new Item(new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64));
     public static final Item ScentedAshes = new Item(new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64));
+    public static final Item WoodSlate = new Item(new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64));
     /**
      * This third array are blocks
      * and itemblocks that are registered
@@ -38,6 +39,7 @@ public class Craftyitemsmod implements ModInitializer {
      * defined here
      */
     public static final Block RosePineBlock = new RosePineBlock();
+    public static final Block WoodCutterBlock = new WoodCutterBlock();
 
     /**
      * Here we define our custom creative
@@ -52,6 +54,8 @@ public class Craftyitemsmod implements ModInitializer {
                 stacks.add(new ItemStack(Craftyitemsmod.ThickString));
                 stacks.add(new ItemStack(Craftyitemsmod.PineDust));
                 stacks.add(new ItemStack(Craftyitemsmod.ScentedAshes));
+                stacks.add(new ItemStack(Craftyitemsmod.WoodCutterBlock));
+                stacks.add(new ItemStack(Craftyitemsmod.WoodSlate));
             })
             .build();
 
@@ -63,12 +67,12 @@ public class Craftyitemsmod implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("craftyitems", "thick_string"), ThickString);
         Registry.register(Registry.ITEM, new Identifier("craftyitems","pine_dust"), PineDust);
         Registry.register(Registry.ITEM, new Identifier("craftyitems","scented_ashes"), ScentedAshes);
+        Registry.register(Registry.ITEM, new Identifier("craftyitems", "wood_slate"), WoodSlate);
 
         // Registering all block objects
         Registry.register(Registry.BLOCK, new Identifier("craftyitems","rose_pine"), RosePineBlock);
         Registry.register(Registry.ITEM, new Identifier("craftyitems", "rose_pine"), new BlockItem(RosePineBlock, new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64)));
-
-        // Adding trasparency to rose pine model
-        // BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), Blocks.RosePineBlock);
+        Registry.register(Registry.BLOCK, new Identifier("craftyitems", "woodcutter"), WoodCutterBlock);
+        Registry.register(Registry.ITEM, new Identifier("craftyitems", "woodcutter"), new BlockItem(WoodCutterBlock, new FabricItemSettings().group(Craftyitemsmod.CRAFTYITEMS_GROUP).maxCount(64)));
     }
 }
---
+++ src/main/java/sweetierick/craftyitemsmod/blocks/WoodCutterBlock.java
@@ -0,0 +1,44 @@
+package sweetierick.craftyitemsmod.blocks;
+
+import net.minecraft.block.*;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+public class WoodCutterBlock extends Block {
+    protected static final VoxelShape SHAPE;
+    public static final DirectionProperty FACING;
+    public WoodCutterBlock() {
+        super(Settings.of(Material.PISTON));
+        this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public boolean hasSidedTransparency(BlockState state) {
+        return true;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    static {
+        FACING = HorizontalFacingBlock.FACING;
+        SHAPE = Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D);
+    }
+}
