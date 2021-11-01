6ce2a691f79f36850bf337ed49c897b059b80417
SweetieRick <65025304+SweetieRick@users.noreply.github.com>
2021-11-01 22:03:16 +0100

    Added baked apple + torch from campfires

---
+++ src/main/java/sweetierick/craftyitemsmod/items/food/BakedAppleItem.java
@@ -0,0 +1,9 @@
+package sweetierick.craftyitemsmod.items.food;
+
+import net.minecraft.item.Item;
+
+public class BakedAppleItem extends Item {
+    public BakedAppleItem(Settings settings) {
+        super(settings);
+    }
+}
