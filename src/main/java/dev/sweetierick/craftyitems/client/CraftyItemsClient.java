61534e3b052ad0194fb2bf112c08bac71318e334
SweetieRick <65025304+SweetieRick@users.noreply.github.com>
2022-08-18 23:50:18 +0200

    Fixed layer cutout not targeting the right classes

--- src/main/java/dev/sweetierick/craftyitems/client/CraftyItemsClient.java
+++ src/main/java/dev/sweetierick/craftyitems/client/CraftyItemsClient.java
@@ -1,21 +1,21 @@
 package dev.sweetierick.craftyitems.client;
 
+import dev.sweetierick.craftyitems.CraftyItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
-import static dev.sweetierick.craftyitems.registry.BlockRegistry.*;
 
 @Environment(EnvType.CLIENT)
 public class CraftyItemsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // ModelLoadingRegistry.INSTANCE.registerResourceProvider(rm -> new CraftyModelProvider());
-        BlockRenderLayerMap.INSTANCE.putBlock(WoodCutterBlock, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(RosePineBlock, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(SmallCookingPotBlock, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(MoguShrineBlock, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(GlassCutterBlock, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(CraftyItems.WoodCutterBlock, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(CraftyItems.RosePineBlock, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(CraftyItems.SmallCookingPotBlock, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(CraftyItems.MoguShrineBlock, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(CraftyItems.GlassCutterBlock, RenderLayer.getCutout());
     }
 }
